9b63c0242c8cabfdbe357883f0d11e1789ed43fe
Roberto Serrano <rserranon@gmail.com>
2021-02-15 21:25:42 -0600

    implemented Layers

---
+++ HAZEL Xcode/Layer.cpp
@@ -0,0 +1,19 @@
+//
+//  Layer.cpp
+//  HAZEL Xcode
+//
+//  Created by Roberto Serrano on 15/02/21.
+//
+
+#include <stdio.h>
+
+#include "Layer.h"
+
+namespace Hazel {
+
+    Layer::Layer(const std::string& debugName)
+        : m_DebugName(debugName)
+    {
+    }
+    
+}
